d27e633805a38172fb200854267d4ee5d04dcbf9
sahhhil5121 <singh.sahil5121@gmail.com>
2025-05-13 14:21:53 +0000

    datatypes basics and comparision

---
+++ basics/comparisons.js
@@ -0,0 +1,19 @@
+//console.log(2 > 1)
+//console.log(2 >= 1)
+//console.log(2 < 1)
+// console.log( 1 == 2)
+//console.log(2 != 1)
+
+//yaha tak sab normal chalta hai 
+
+//console.log(null > 0)
+//console.log(null == 0)
+//console.log(null >= 0)
+
+//console.log(undefined > 0)
+//console.log(undefined == 0)
+//console.log(undefined < 0)
+
+// problem yaha se shuru hoa hai jab hmlog null ya undefined ko compare karne lagte hai ya fir different type of datatype
+// compare karne lagte hai
+
--- basics/datatypes.js
+++ basics/datatypes.js
@@ -2,3 +2,19 @@
 
 // alert ( 2 + 3) node mei kam nahi krta ye....html ua browser ka inspect wala mei karta hai // 
 
+// overall 2 type of datatypes mei sabko divide kiya gaya hai
+
+// PRIMITIVE (CALL BY VALUE)
+// 7 types - number, boolean, symbol , string, null , undefined, bigInt .
+
+//NON - PRIMIOTIVE (CALL BY REFERENCE)
+// array, objects, functions
+
+// javaScript is a dynamically typed language, which means that data types of variables are determined by the value they hold
+//  at runtime and can change throughout the program as we assign different values to them.
+
+
+//const id = Symbol("123")
+//const anotherId = Symbol("123")
+
+//console.log(id === anotherId) => false
