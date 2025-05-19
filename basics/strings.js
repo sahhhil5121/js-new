ba8f1da7cf9116fafc61b8c4b7ad8a8300c23e2c
sahhhil5121 <singh.sahil5121@gmail.com>
2025-05-19 13:57:59 +0000

    strings basics

---
+++ basics/strings.js
@@ -0,0 +1,29 @@
+const name = "sahil"
+const Count = " 50 "
+
+//console.log(name + Count + "value")
+
+//console.log(`hello my name is ${name} and my repo count is ${Count}`)
+
+const gameName = new String ('sahil')
+
+//console.log(gameName[0])
+//console.log(gameName.__proto__)
+//console.log(gameName.toUpperCase())
+//console.log(gameName.charAt(2))
+//console.log(gameName.indexOf('i'))
+
+const newString = gameName.substring(0,4)
+//console.log(newString)
+
+const anotherString = gameName.slice(-5,7)   // slice kaise work karta h????????(reverse se count karta h pata hai lekin kaise???)
+//console.log(anotherString)
+
+const newStr = "  sahil   "
+//console.log(newStr.trim())          // TRIM extra spaces jo nahi chahiye hota hai usko hata deta hai
+
+const url = "sahil/singh.com"
+//console.log(url.replace('singh','5121'))
+
+//console.log(url.includes('scsc'))
+
