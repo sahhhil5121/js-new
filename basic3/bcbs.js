d3ee8ac2812918e0a1bbe13cb425be6ddeaf1f40
sahhhil5121 <singh.sahil5121@gmail.com>
2025-08-31 12:45:09 +0000

    fnqpfqe

--- basic3/bcbs.js
+++ basic3/bcbs.js
@@ -6,3 +6,12 @@ const obj4 = {5: "a", 6: "b"}
 const obj3 = Object.assign({}, obj1, obj2, obj4)   // o/p - { '1': 'a', '2': 'b', '3': 'a', '4': 'b', '5': 'a', '6': 'b' }
 
 console.log(obj3)
+
+const obj5 = {1: "a", 2: "b"}
+const obj7 = {3: "a", 4: "b"}
+const obj8 = {5: "a", 6: "b"}
+
+//const obj3 = { obj1, obj2 }                      // o/p- { obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'a', '4': 'b' } }
+const obj9 = Object.assign({}, obj1, obj2, obj4)   // o/p - { '1': 'a', '2': 'b', '3': 'a', '4': 'b', '5': 'a', '6': 'b' }
+
+console.log(obj3)
